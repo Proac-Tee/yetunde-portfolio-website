ee0cf91f54fa7f214fd21a01b6da9938ec49d387
onlycodes <babatundetaiwo038@gmail.com>
2022-09-24 23:12:33 +0100

    feat: profile summary added

--- src/components/profiles/Home.js
+++ src/components/profiles/Home.js
@@ -2,7 +2,18 @@ import React from "react";
 import classes from "./Home.module.css";
 
 const Home = () => {
-  return <section></section>;
+  return (
+    <section>
+      <div>
+        <h1>
+          Emotionally intelligent and innovative, History and International
+          Studies graduate with experience in customer service and
+          administration. Good relational skills with a flair for supporting
+          others and ensuring the organization runs efficiently.
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
