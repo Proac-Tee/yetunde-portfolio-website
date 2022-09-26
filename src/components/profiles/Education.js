117de4ea34dc8d0296015b92abda5725a438b380
onlycodes <babatundetaiwo038@gmail.com>
2022-09-26 21:54:27 +0100

    feat: Education component added

---
+++ src/components/profiles/Education.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "./Education.module.css";
+
+const Education = () => {
+  return (
+    <section>
+      <div></div>
+    </section>
+  );
+};
+
+export default Education;
