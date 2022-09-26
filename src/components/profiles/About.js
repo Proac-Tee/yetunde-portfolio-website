336e24a03b400308c7afde5121f620f91549463b
onlycodes <babatundetaiwo038@gmail.com>
2022-09-26 21:13:58 +0100

    feat: About summary added

--- src/components/profiles/About.js
+++ src/components/profiles/About.js
@@ -3,7 +3,16 @@ import React from "react";
 import classes from "./About.module.css";
 
 const About = () => {
-  return <section></section>;
+  return <section>
+        <div>
+        <h1>
+          Emotionally intelligent and innovative, History and International
+          Studies graduate with experience in customer service and
+          administration. Good relational skills with a flair for supporting
+          others and ensuring the organization runs efficiently.
+        </h1>
+      </div>
+  </section>;
 };
 
 export default About;
--- src/components/profiles/Home.js
+++ src/components/profiles/Home.js
@@ -4,14 +4,7 @@ import classes from "./Home.module.css";
 const Home = () => {
   return (
     <section>
-      <div>
-        <h1>
-          Emotionally intelligent and innovative, History and International
-          Studies graduate with experience in customer service and
-          administration. Good relational skills with a flair for supporting
-          others and ensuring the organization runs efficiently.
-        </h1>
-      </div>
+
     </section>
   );
 };
