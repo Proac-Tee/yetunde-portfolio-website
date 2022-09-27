04ba7e168a5694baa4b255bad550320c17c28330
onlycodes <babatundetaiwo038@gmail.com>
2022-09-27 21:28:50 +0100

    feat: Contact details added

--- src/components/profiles/Contact.js
+++ src/components/profiles/Contact.js
@@ -3,7 +3,14 @@ import React from "react";
 const Contact = () => {
   return (
     <section>
-        <h1>This is the contact page</h1>
+      <h1>Contact Me</h1>
+      <div>
+        <ul>
+          <li>07065002789</li>
+          <li>08156843902</li>
+          <l1>oladunniyetunde6@gmail.com</l1>
+        </ul>
+      </div>
     </section>
   );
 };
