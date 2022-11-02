0ab778c24cdcfbea48052d94d08463c06e404e26
onlycodes <babatundetaiwo038@gmail.com>
2022-11-02 13:21:04 +0000

    Styles: Skills component style updated

--- src/components/profiles/Skills.js
+++ src/components/profiles/Skills.js
@@ -4,11 +4,14 @@ import classes from "./Skills.module.css";
 const Skills = () => {
   return (
     <Fragment>
-      <ul>
-        <li>Decision making skill</li>
-        <li>Organizational skill</li>
-        <li>Leadership skill</li>
-      </ul>
+      <div className={classes.container}>
+        <h1>Skills</h1>
+        <ul>
+          <li>Decision making skill</li>
+          <li>Organizational skill</li>
+          <li>Leadership skill</li>
+        </ul>
+      </div>
     </Fragment>
   );
 };
